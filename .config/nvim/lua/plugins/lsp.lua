f1efd335c768084e6e7310de41c2300dd24ca347
Ciaron Rider <ciaron@ridernator.com>
2025-04-20 12:42:17 +0100

    Add methods for open header in tab / vsplit

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -61,10 +61,54 @@ return {
       })
       vim.lsp.buf.rename()
     end, {silent = true})
+
     vim.keymap.set('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', {silent = true})
     vim.keymap.set('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>', {silent = true})
     vim.keymap.set('n', 'gr', ':lua vim.lsp.buf.references()<CR>', {silent = true})
     vim.keymap.set('n', 'ga', ':lua vim.lsp.buf.code_action()<CR>', {silent = true})
     vim.keymap.set('n', 'gh', ':ClangdSwitchSourceHeader<CR>', {silent = true})
+
+    vim.keymap.set("n", "gH", function()
+      local util = require 'lspconfig.util'
+
+      local method_name = 'textDocument/switchSourceHeader'
+      local client = vim.lsp.get_clients({ bufnr = 0, name = 'clangd' })[1]
+      if not client then
+        return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
+      end
+      local params = vim.lsp.util.make_text_document_params(0)
+      client.request(method_name, params, function(err, result)
+        if err then
+          error(tostring(err))
+        end
+        if not result then
+          vim.notify('corresponding file cannot be determined')
+          return
+        end
+        vim.cmd.tabedit(vim.uri_to_fname(result))
+      end, 0)
+    end, {silent = true})
+
+    vim.keymap.set("n", "g<C-h>", function()
+      local util = require 'lspconfig.util'
+
+      local method_name = 'textDocument/switchSourceHeader'
+      local client = vim.lsp.get_clients({ bufnr = 0, name = 'clangd' })[1]
+      if not client then
+        return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
+      end
+      local params = vim.lsp.util.make_text_document_params(0)
+      client.request(method_name, params, function(err, result)
+        if err then
+          error(tostring(err))
+        end
+        if not result then
+          vim.notify('corresponding file cannot be determined')
+          return
+        end
+        vim.cmd("vsplit")
+        vim.cmd.edit(vim.uri_to_fname(result))
+      end, 0)
+    end, {silent = true})
   end
 }
