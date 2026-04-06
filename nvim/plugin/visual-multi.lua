45f1e578da2974bce086ecbce563f5e93cf5a30f
Ciaron Rider <ciaron@ridernator.com>
2026-04-06 13:42:08 +0100

    Commented out visual-multi until it is sorted

--- nvim/plugin/visual-multi.lua
+++ nvim/plugin/visual-multi.lua
@@ -1,6 +1,8 @@
-vim.pack.add({ 'https://github.com/mg979/vim-visual-multi' })
- 
-vim.g.VM_default_mappings = 0
-
-vim.keymap.set("n", "<c-s-Up>",   "<Plug>(VM-Add-Cursor-Up)",   {silent = true})
-vim.keymap.set("n", "<c-s-Down>", "<Plug>(VM-Add-Cursor-Down)", {silent = true})
+-- vim.pack.add({ 'https://github.com/mg979/vim-visual-multi' })
+--
+-- require('vim-visual-multi').setup()
+--
+-- vim.g.VM_default_mappings = 0
+--
+-- vim.keymap.set("n", "<c-s-Up>",   "<Plug>(VM-Add-Cursor-Up)",   {silent = true})
+-- vim.keymap.set("n", "<c-s-Down>", "<Plug>(VM-Add-Cursor-Down)", {silent = true})
