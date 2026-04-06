7bbe6a0cec259fb1c388e67669e6a0738f6c1838
Ciaron Rider <ciaron@ridernator.com>
2026-04-06 13:45:47 +0100

    Added markdown plugin

--- nvim/plugin/filetree.lua
+++ nvim/plugin/filetree.lua
@@ -1,5 +1,6 @@
 vim.pack.add({ 'https://github.com/nvim-tree/nvim-web-devicons',
                'https://github.com/nvim-tree/nvim-tree.lua' })
+
 require('nvim-tree').setup()
 
 vim.keymap.set("n", "gt", ":NvimTreeToggle<CR>", { silent = true })
---
+++ nvim/plugin/markdown.lua
@@ -0,0 +1,5 @@
+vim.pack.add({ 'https://github.com/nvim-tree/nvim-web-devicons',
+               'https://github.com/nvim-treesitter/nvim-treesitter',
+               'https://github.com/MeanderingProgrammer/render-markdown.nvim' })
+
+require('render-markdown').setup()
