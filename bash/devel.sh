a02afad730c069733fbb06affab5bf2f277cf367
Ciaron Rider <ciaron@ridernator.com>
2026-04-28 23:16:24 +0100

    Fixes to session management

--- bash/devel.sh
+++ bash/devel.sh
@@ -1,3 +1,21 @@
 alias gdb="gdb -ex=\"set confirm off\""
 
-alias devMode="tmux new-session -c "${HOME}" -s home"
+dev() {
+  if ! tmux has-session -t home &> /dev/null; then
+    tmux new-session -d -c "${HOME}" -s home
+  fi
+
+  if test -n "$TMUX"; then
+    tmux switch-client -t home
+  else
+    while tmux attach-session -d -t home 2> /dev/null; do
+      :
+    done
+
+    local -r sessionCount="$(tmux list-sessions 2> /dev/null | wc --lines)"
+
+    if test $sessionCount -gt 0; then
+      echo "$sessionCount sessions left alive"
+    fi
+  fi
+}
